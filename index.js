ae0491e40eaea81b8b90e4cdb3a5ba5cd280bea6
Jason Starkman <jrstarkman@gmail.com>
2023-06-06 17:12:03 -0400

    Exercise 3.11

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('build'))
+
 morgan.token('req-body', (req) => JSON.stringify(req.body));
 
 app.use(morgan(':method :url :status :response-time ms :req-body'))
