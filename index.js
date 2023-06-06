8e21cd367a1010d35f36604ec615bd2d93ee06c2
Jason Starkman <jrstarkman@gmail.com>
2023-06-06 14:59:13 -0400

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -94,6 +97,6 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
